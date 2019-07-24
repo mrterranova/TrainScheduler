a28a8faffba3b1a276bdccc8090a21c1e0f699ef
Michal Terranova <michalterranova@mail.com>
2019-07-24 10:33:18 -0400

    completed assignment

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -38,8 +38,6 @@ function currentTime() {
 };
 
 
-
-
 //when click submit button hide application
 $("#submitBtn").on("click", function () {
     $(".application").hide();
@@ -48,35 +46,32 @@ $("#submitBtn").on("click", function () {
     //collect input values by the user for following
     name = $("#userName").val().trim();
     destination = $("#userDestination").val().trim();
-    firstTrainTime = moment($("#userArrival").val().trim(), "HH:mm").subtract(10, "years").format("X");
-    frequency = $("#userFrequency").val().trim();
+    firstTrainTime = $("#userArrival").val().trim();
+    frequency = parseInt($("#userFrequency").val().trim());
     //create a new variable for time
     nextTrainTime;
 
-    console.log(firstTrainTime);
+    console.log("hello", firstTrainTime);
     
     //if user leaves all fields blank then close application. 
     if (name === "" || destination === "" || firstTrainTime === "" || frequency === "") {
-        alert("Information invalid. You're train can not be scheduled at this time.");
+        alert("Information invalid. Your train can not be scheduled at this time.");
 
     }
     else {
         //consistency with page to uppercase
         name = name.toUpperCase();
         destination = destination.toUpperCase();
-        frequency = frequency.toUpperCase();
         
         console.log(name);
 
-
-
-        //now find first train appearance from user input, make sure that it is after current time.
-        var formatTime = moment().min(moment(firstTrainTime, "HH:mm"));
-        console.log(formatTime.format("HH:mm"));
-        if (firstTrainTime < moment("HH:mm") || firstTrainTime === moment("HH:mm")) {
-            firstTrainTime = firstTrainTime + frequency;
-            firstTrainTime = nextTrainTime;
-        }
+        // //now find first train appearance from user input, make sure that it is after current time.
+        // var formatTime = moment().min(moment(firstTrainTime, "HH:mm"));
+        // console.log(formatTime.format("HH:mm"));
+        // if (firstTrainTime < moment("HH:mm") || firstTrainTime === moment("HH:mm")) {
+        //     firstTrainTime = firstTrainTime + frequency;
+        // }
+        // console.log(firstTrainTime)
         
         //push information to the database
         database.ref().push({
@@ -104,15 +99,26 @@ database.ref().on("child_added", function(snapshot){
     var destination = snapshot.val().trainDestination;
     var frequency = snapshot.val().trainFrequency;
     var trainTime = snapshot.val().trainArrival;
+    var trainTimeArray = trainTime.split(":");
+    
+    var firstTrainTimeToday = moment().hour(trainTimeArray[0]).minutes(trainTimeArray[1]);
+    console.log("firstTrainTimeToday", firstTrainTimeToday);
 
-    console.log("train",trainTime);
+    var trainTimeDifference = Math.abs(moment().diff(firstTrainTimeToday, "minutes"));
 
-    var remainder = moment().diff(moment().unix(trainTime), "minutes") % frequency;
+    console.log("Traintime Diff", trainTimeDifference)
+
+    console.log("frequency", frequency);
+
+    var remainder = trainTimeDifference % frequency;
     var minutes = frequency - remainder;
+    
+    console.log("minutes until next train", minutes);
+
     var arrival = moment().add(minutes, "m").format("HH:mm");
     
     if (minutes ===1){
-        alert("Final Boarding Call for "+ name);
+        // alert("Final Boarding Call for "+ name);
     }
     //append following information to new row in table
     newDiv = $("tbody").append("<tr>");
